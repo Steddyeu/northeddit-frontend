f8dd1785df38ef67db008c53f47c59268d02cb94
Ed <edwardaacowell@gmail.com>
2020-12-07 15:09:15 +0000

    added nav link into the app

--- nc-news/src/App.js
+++ nc-news/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Header from './components/Header';
+import Nav from './components/Nav';
 
 function App() {
   return (
     <div className="App">
     <Header />
+    <Nav />
     </div>
   );
 }
